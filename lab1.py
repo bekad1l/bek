8964cf84515868c1764ac46ba33ffde135ae389d
bekad1l <b_mustafa@kbtu.kz>
2024-01-23 21:11:08 +0600

    lab1

--- lab1.py
+++ lab1.py
@@ -1,3 +1,264 @@
+#Example 1
+if 5 > 2:
+  print("Five is greater than two!")
+#Example 2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+#example 3
+#This is a comment
+print("Hello, World!")
+
+#example 4
+print("Hello, World!") #This is a comment
+
+#example 5
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#example 6
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#example 7
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+#example 8 - variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#example 9 - variables
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#example 10 - variables
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#example 11 - varibales
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#example 12 - varibales
+x = "John"
+# is the same as
+x = 'John'
+
+#example 13 - varibales 
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+#example 14 - variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#example 15 - multiple values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#example 16 - multiple values 
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#example 17 - multiple values
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#example 18 - output varibales 
+x = "Python is awesome"
+print(x)
+
+#example 19 - output variables
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#example 20 - output variables
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+#example 21
+x = 5
+y = 10
+print(x + y)
+
+#example 22
+x = 5
+y = "John"
+print(x, y)
+
+#example 23 - global variables
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#example 24 - global varibales
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+#example 25 - data types 
+x = 5
+print(type(x))
+
+#example 26 - numbers
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#example 27 - numbers 
+print(type(x))
+print(type(y))
+print(type(z))
+
+#example 28 - numbers
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#example 29 - numbers 
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#example 30 
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#example 31 
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#example 32 
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+#example  33 - strings 
+a = "Hello, World!"
+print(a[1])
+
+#example 34 - strings 
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+#example 35 - strings
+b = "Hello, World!"
+print(b[2:5])
+
+#example 36 - strings
+b = "Hello, World!"
+print(b[:5])
+
+#example 37 - strings 
+b = "Hello, World!"
+print(b[2:])
+
+#example 38 - strings 
+a = "Hello, World!"
+print(a.upper())
+
+#example 39 - strings 
+a = "Hello, World!"
+print(a.lower())
+
+#example 40 - strings 
+a = " Hello, World! "
+print(a.strip())
+
+#example 41 - strings 
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#example 42 - strings 
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+#example 43 - strings 
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#example 44 - strings 
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#example 45 - strings 
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+#example 46 - strings 
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+#example 47 - strings 
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+
+
+
+
 #Python Syntax exercise 1
 print("Hello world")
 
